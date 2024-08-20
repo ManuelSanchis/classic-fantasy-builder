b921a23cc529b4af9a652c8a29db254998d3d8c3
Manuel Sanchis <manuelsanchistormo@gmail.com>
2024-08-20 20:30:31 +0200

    feat: Update shared.module.ts
    Add RouterModule to imports

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  imports: [],
+  imports: [
+    RouterModule,
+  ],
   exports: [
     FooterComponent,
     HeaderComponent,
